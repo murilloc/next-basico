8c0072b421c14e62813adca53b66ede811fc02a5
Murillo Cesar <murillo.devops@gmail.com>
2023-10-27 11:14:34 -0300

    api com multiplos parametros

---
+++ src/pages/api/form.js
@@ -0,0 +1,28 @@
+const usuarios = []
+export default function form(req, res) {
+
+    if (req.method === 'POST') {
+        post(req, res)
+    } else if (req.method === 'GET') {
+        get(req, res)
+    } else {
+        res.status(405).json({
+            message: 'We only support POST'
+        })
+    }
+
+    res.status(200).json({
+        dados: JSON.parse(req.body)
+    })
+
+}
+
+function post(req, res) {
+    const usuario = JSON.parse(req.body)
+    usuarios.push(usuario)
+    res.status(200).send()
+}
+
+function get(req, res) {
+    res.status(200).json(usuarios)
+}
---
+++ src/pages/form.jsx
@@ -0,0 +1,55 @@
+import {useState} from "react";
+
+export default function form() {
+
+    const [nome, setNome] = useState('')
+    const [idade, setIdade] = useState(0)
+    const [usuarios, setUsuarios] = useState([])
+
+    async function salvarUsuario() {
+        await fetch('/api/form', {
+            method: 'POST',
+            body: JSON.stringify({
+                nome,
+                idade
+            })
+        })
+
+        const resposta = await fetch('/api/form', {
+            method: 'GET'
+        })
+
+        const usuarios = await resposta.json()
+        setUsuarios(usuarios)
+
+        setIdade(0)
+        setNome('')
+
+    }
+
+    function renderizarUsuarios() {
+        return usuarios.map((usuario, index) => {
+            return <li key={index}>{usuario.nome} tem {usuario.idade} anos.</li>
+        })
+    }
+
+    return (
+        <div>
+            <h1>Integrando com a API #2</h1>
+            <div>
+                <input type="text" value={nome} onChange={(e) => setNome(e.target.value)}/>
+            </div>
+            <div>
+                <input type="number" value={idade} onChange={(e) => setIdade(+e.target.value)}/>
+            </div>
+            <button onClick={salvarUsuario}>Enviar</button>
+
+
+            <ul>
+                {renderizarUsuarios()}
+            </ul>
+
+
+        </div>
+    )
+}
